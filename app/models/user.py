6baf2651a55fd1d18f249e9a466897d3ffb72bdb
Elias Raffaelli <eliasraffaelli@gmail.com>
2021-10-14 03:15:50 -0300

    modelo de usuario actualizado y modelo de rol creado

--- app/models/user.py
+++ app/models/user.py
@@ -2,15 +2,18 @@ from sqlalchemy import Column, Integer, String
 from app.db import db
 
 class User(db.Model):
-    __tablename__= "users"
+    __tablename__= "ususarios"
     id = Column(Integer, primary_key=True)
+    email = Column(String (30), unique=True)
+    username = Column(String (30), unique=True)
+    password = Column(String (30), unique=True)
     first_name = Column(String (30), unique=True)
     last_name = Column(String (30), unique=True)
-    email = Column(String (30), unique=True)
-    password = Column(String (30), unique=True)
 
-def __init__(self, first_name=None, last_name=None, email=None, password=None):
+
+def __init__(self, email=None, username=None, password=None, first_name=None, last_name=None):
+    self.email = email
+    self.username = username
+    self.password = password
     self.first_name = first_name
     self.last_name = last_name
-    self.email = email
-    self.password = password

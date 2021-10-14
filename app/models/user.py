31351bfba82e1f8bc9eb9194a4bbf46fd1fa74f2
Elias Raffaelli <eliasraffaelli@gmail.com>
2021-10-14 13:33:10 -0300

    modelo de datos de  usuario finalizado

--- app/models/user.py
+++ app/models/user.py
@@ -1,19 +1,24 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
 from app.db import db
 
 class User(db.Model):
-    __tablename__= "ususarios"
+    __tablename__= "usuarios"
     id = Column(Integer, primary_key=True)
     email = Column(String (30), unique=True)
     username = Column(String (30), unique=True)
-    password = Column(String (30), unique=True)
-    first_name = Column(String (30), unique=True)
-    last_name = Column(String (30), unique=True)
+    password = Column(String (30))
+    activo = Column(Integer)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    first_name = Column(String (30))
+    last_name = Column(String (30))
 
 
-def __init__(self, email=None, username=None, password=None, first_name=None, last_name=None):
+def __init__(self, email=None, username=None, activo=None, password=None, first_name=None, last_name=None):
     self.email = email
     self.username = username
     self.password = password
+    self.activo = activo
     self.first_name = first_name
     self.last_name = last_name

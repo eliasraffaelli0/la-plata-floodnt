96b8d672b28183dcda30d317705d7faea11dedf3
Elias Raffaelli <eliasraffaelli@gmail.com>
2021-10-22 17:44:28 -0300

    modelo de las relaciones actualizado y la query de permisoChecker ahora está hecha utilizando any()

--- app/models/rol.py
+++ app/models/rol.py
@@ -1,10 +1,20 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.db import db
+from sqlalchemy.orm import relationship
+from app.models.permiso import Permiso
+
+association_table = db.Table('rol_tiene_permiso', db.metadata,
+    Column('usuario_id', ForeignKey('roles.id'), primary_key=True),
+    Column('rol_id', ForeignKey('permisos.id'), primary_key=True)
+)
 
 class Rol(db.Model):
     __tablename__= "roles"
     id = Column(Integer, primary_key=True)
     name = Column(String (30), unique=True)
+    permisos = relationship(Permiso,
+                    secondary=association_table,
+                    backref="roles")
 
     def __init__(self, name=None):
         self.name = name

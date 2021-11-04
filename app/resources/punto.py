60425881fa043dc2d8bf7957303e3c23a4637498
Lucaroni Yanasú <ylucaroni@gmail.com>
2021-11-04 19:52:19 -0300

    Se modificó la lógica de la creacion de puntos y se agregó la clase PuntoValidator

--- app/resources/punto.py
+++ app/resources/punto.py
@@ -2,7 +2,8 @@ from flask import redirect, render_template, request, url_for, session, abort, f
 from app.db import db
 from app.helpers.auth import authenticated
 from app.models.punto import Punto
-from app.validators.puntoDuplicateValidator import puntoDuplicateChecker
+from app.validators.puntoValidator import PuntoValidator
+from app.helpers.permisoValidator import permisoChecker
 from sqlalchemy.sql import text
 import re
 
@@ -25,8 +26,12 @@ def index():
 
 
 def new():
-
-    return render_template("puntos/new.html")
+    if not authenticated(session):
+        abort(401)
+    if not permisoChecker(session, "user_index"):
+        abort(401)
+    errors = {}
+    return render_template("puntos/new.html", errors=errors)
 
 
 def create():
@@ -34,24 +39,18 @@ def create():
         abort(401)
 
     new_punto = Punto(**request.form)
+    errors = {}
+    errors = PuntoValidator(new_punto).validate_create()
 
-    # raw string utilizado para validar que se trate de un mail
-    regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Z|a-z]{2,}\b"
-    if not (re.fullmatch(regex, new_punto.email)):
-        flash("Ingrese un mail válido.")
-        return redirect(url_for("puntos_new"))
-
-    if puntoDuplicateChecker(Punto.name, new_punto.name, "nombre"):
-        return redirect(url_for("puntos_new"))
-
-    if puntoDuplicateChecker(Punto.coordenadas, new_punto.coordenadas, "coordenadas"):
-        return redirect(url_for("puntos_new"))
+    if errors:
+        return render_template("puntos/new.html", errors=errors, fieldsInfo=new_punto)
 
     if (
         new_punto.name == ""
-        or new_punto.direccion == ""
-        or new_punto.coordenadas == ""
-        or new_punto.telefono == ""
+        or new_punto.address == ""
+        or new_punto.latitude == ""
+        or new_punto.longitude == ""
+        or new_punto.telephone == ""
         or new_punto.email == ""
     ):
         flash("Debe completar todos los campos")

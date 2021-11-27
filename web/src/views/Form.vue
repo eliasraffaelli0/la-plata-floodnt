026d3821ff7c91d644aaab4299c2dc5b973e1512
Lucaroni Yanasú <ylucaroni@gmail.com>
2021-11-27 13:05:23 -0300

    Se agregó un mapa de prueba

---
+++ web/src/views/Form.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <l-map style="height: 700px" :zoom="zoom" :center="center" @click="onClick">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+  </l-map>
+</div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      zoom: 14,
+      center: [-34.9187,-57.956],
+      markerLatLng: [],
+    };
+  },
+  methods: {
+    onClick(e){
+      if(e.latlng){
+        this.markerLatLng = e.latlng;
+      }
+      console.log(e);
+  }
+}
+}
+</script>

27e19ecb4191cf8097981f4dfce5330fa7f327a7
Elias Raffaelli <eliasraffaelli@gmail.com>
2022-01-28 16:38:55 -0300

    schema del api de reports creado

---
+++ app/schema/reportSchema.py
@@ -0,0 +1,25 @@
+from marshmallow import Schema, fields
+
+
+class ReportSchema(Schema):
+    title = fields.String()
+    latitude = fields.String()
+    longitude = fields.String()
+    category = fields.String()
+    complainant_name = fields.String()
+    complainant_last_name = fields.String()
+    complainant_email = fields.String()
+    description = fields.String()
+
+
+class ReportPaginationSchema(Schema):
+    page = fields.Int()
+    per_page = fields.Int()
+    pages = fields.Int()
+    total = fields.Int()
+    items = fields.Nested(ReportSchema, many=True, data_key="reports")
+
+
+reportss_schema = ReportSchema(many=True)
+report_schema = ReportSchema()
+report_pagination_schema = ReportPaginationSchema()

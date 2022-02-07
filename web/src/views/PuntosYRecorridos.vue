e5de8d05fd96a5f7adeb2b2a467cac0ec06c70bb
Lucaroni Yanasú <ylucaroni@gmail.com>
2022-02-07 00:18:45 -0300

    .

--- web/src/views/PuntosYRecorridos.vue
+++ web/src/views/PuntosYRecorridos.vue
@@ -148,7 +148,7 @@ export default {
 
     async fetchRoutes(page) {
       const response = await axios.get(
-        `https://admin-grupo5.proyecto2021.linti.unlp.edu.ar/api/puntos/?page=${page}`
+        `https://admin-grupo5.proyecto2021.linti.unlp.edu.ar/api/recorridos/?page=${page}`
         // `http://127.0.0.1:5000/api/recorridos/?page=${page}`
       );
       response.data.recorridos.forEach((recorrido) => {
